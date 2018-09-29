e1ac46f4aefd174157443d2eda779226c2301b30
Sivallia <jxfriebel@gmail.com>
2018-09-29 18:15:28 +0200

    3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,95 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { CalendarModule } from "ion2-calendar";
-import { CalendarComponentOptions } from 'ion2-calendar'
+import { CalendarComponentOptions } from 'ion2-calendar';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  type: 'string';
-  format: 'string';
-  dateMulti: string[];
-  optionsMulti: CalendarComponentOptions = {
-    pickMode: 'multi'
+
+  public type: 'string';
+  dateMulti: String[] = [];
+  public optionsMulti: CalendarComponentOptions = {
+    pickMode: 'multi',
+    daysConfig: [
+      {
+        date: new Date(2018,0,10),
+        marked: true,
+        subTitle: "how are you",
+      }
+    ]
   }
 
-  constructor(public navCtrl: NavController) {
+  ueberfluegeProTag = {}
+  ueberfluegeAnzahl = {}
 
+  constructor(public navCtrl: NavController) {
+    console.log(this.rawData['info']['satname']);
+    for (let overpass of this.rawData['passes']) {
+      console.log(overpass['startUTC']);
+    }
   }
   onChange($event) {
     console.log($event);
   }
+
+  rawData = {
+    "info": {
+      "satid": 25544,
+      "satname": "SPACE STATION",
+      "transactionscount": 4,
+      "passescount": 3
+    },
+    "passes": [
+      {
+        "startAz": 307.21,
+        "startAzCompass": "NW",
+        "startEl": 13.08,
+        "startUTC": 1521368025,
+        "maxAz": 225.45,
+        "maxAzCompass": "SW",
+        "maxEl": 78.27,
+        "maxUTC": 1521368345,
+        "endAz": 132.82,
+        "endAzCompass": "SE",
+        "endEl": 0,
+        "endUTC": 1521368660,
+        "mag": -2.4,
+        "duration": 485
+      },
+      {
+        "startAz": 311.56,
+        "startAzCompass": "NW",
+        "startEl": 50.94,
+        "startUTC": 1521451295,
+        "maxAz": 37.91,
+        "maxAzCompass": "NE",
+        "maxEl": 52.21,
+        "maxUTC": 1521451615,
+        "endAz": 118.61,
+        "endAzCompass": "ESE",
+        "endEl": 0,
+        "endUTC": 1521451925,
+        "mag": -2,
+        "duration": 325
+      },
+      {
+        "startAz": 291.06,
+        "startAzCompass": "WNW",
+        "startEl": 3.47,
+        "startUTC": 1521457105,
+        "maxAz": 231.58,
+        "maxAzCompass": "SW",
+        "maxEl": 14.75,
+        "maxUTC": 1521457380,
+        "endAz": 170.63,
+        "endAzCompass": "S",
+        "endEl": 0,
+        "endUTC": 1521457650,
+        "mag": -0.1,
+        "duration": 485
+      }
+    ]
+  };
 }
