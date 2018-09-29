3a610c4fe805decae77bb6d621921f9e610a21b2
Sivallia <jxfriebel@gmail.com>
2018-09-29 13:33:23 +0200

    2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CalendarModule } from "ion2-calendar";
+import { CalendarComponentOptions } from 'ion2-calendar'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  type: 'string';
+  format: 'string';
+  dateMulti: string[];
+  optionsMulti: CalendarComponentOptions = {
+    pickMode: 'multi'
+  }
 
   constructor(public navCtrl: NavController) {
 
   }
-
+  onChange($event) {
+    console.log($event);
+  }
 }
